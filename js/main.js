2270d25e3dc1b6b8619c0bca5dd9cbba996e4dc1
Caseres Marina <marinacaseres1997@gmail.com>
2021-10-03 14:45:58 -0300

    Agregado estilo.

--- js/main.js
+++ js/main.js
@@ -1,55 +1,73 @@
-let viewControl = new ViewControl();
+//div del DOM que contiene mensaje del jugador ganador
 let div = document.getElementById("winner");
+//h1 del DOM que mostrará el jugador ganador
 let h1 = document.getElementById("winnerPlayer");
+//parrafo del DOM que muestra si hay un color elejido que ya esté en uso en las piezas del otro jugador
 let pColor = document.getElementById("pColor");
+//parrafo del DOM que va a indicar que jugador tiene el turno
 let pStartPlayer = document.getElementById("pStartPlayer");
+//canvas del DOM don de se pinta el tablero y las fichas
 let canvas = document.getElementById("canvas");
 /** @type {CanvasRenderingContext2D} */
 let ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth ;
-canvas.height = window.innerHeight-20;
+//setamos el tamaño del canvas a tamaño del la pantalla window
+canvas.width = window.innerWidth ;//document.documentElement.clientWidth,
+canvas.height = window.innerHeight-20;//document.documentElement.clientHeight
 let width = canvas.width;
 let height = canvas.height;
-
+//formulario del DOM para cambbiar la dimensión del tablero
 let formDimension = document.getElementById("dimensionBoard");
+//leemos la dimensión por defecto desde el DOM
 let dimension = Number(extractDimension(formDimension));
+//se calcula la cantidad de piezas por jugador dependiendo de as dimensiones del tablero
 let cantPieceByPlayer = (dimension*dimension)/2;
+//se establece el radio de las piezas, ya que son creadas a partir de un círculo.
 let radio= 20;
+//se establece el tamaño de las celdas del tablero
 let pixels = 70;
-
-//Este es el tamaño de la imagen
+//establece el tamaño de la imagen de las piezas
 const piecePixel = radio*2;
-
-//se le pasa width y height para centrarlo.
+//se les pasa la dimensión del tablero
+//se les pasa width y height para centrarlo.
+//se les pasa los pixels de cada celda.
+//parte de atras del tablero
 let board = new Board(dimension, dimension, ctx, width, height, pixels);
+//parte frontal del tablero
 let frontBoard = new FrontBoard(dimension, dimension, ctx, width, height, pixels);
+//matrix que contiene las celdas del tablero
 let matrixBoard = new MatrixBoard(dimension, dimension, ctx, width, height, pixels);
+//se inicia una variable que maneja el comportamiento del juego
+//usado para buscar el ganador
 let game = new Game(matrixBoard);
-/*let ceee = matrixBoard.getCells();
-console.log(ceee);*/
-//console.log(ceee[1].getPiece());
-
-//constantes
+//esta variable guarda el comportamiento de vista de los distintos elementos del DOM
+let viewControl = new ViewControl();
+//variable que determina si el juego comenzó
 let start = false;
+//Array de almacenamiento de las piezas del juego
 let pieces = [];
+//formulario del DOM de colores de piezas de los jugadores
 let from1 = document.getElementById("formColorPlayer1");
 let from2 = document.getElementById("formColorPlayer2");
+//variables donde se guardan los respectivos colores de las piezas
 let fill1 = viewControl.readColor(from1);
 let fill2 = viewControl.readColor(from2);
+//variable que guarda la pieza anterior clicleada
 let lastClickedPiece = null;
+//variable boolean que guarda si el mause hizo click
 let isMouseDown = false;
+//variables para controlar turnos
 //en caso de comenzar el juego por primera vez se setea
 //que el ultimo turno fue el del jugador 2, asi puede empezar el jugador 1
 let lastTurn = 2;
 let newTurn;
-
+//se comienza dibujando la parte de atras del tablero
 board.draw();
-//arreglar que se pinten las dos partes del tablero al iniciar
+//se dibuja la parte frontal del tablero
 frontBoard.draw();
-
+//imagen para las piezas
 image = new Image(); //iniciar ruta
 image.src="img/piece.png";
-
+//imagen para la parte frontal del tablero
 imageBoard = new Image(); //iniciar ruta
 imageBoard.src="img/ventana.png";
 
@@ -61,7 +79,7 @@ imageBoard.onload = function(){
     frontBoard.setImage(imageBoard);
     frontBoard.draw();
 }
-
+//extrae el valor del formilario de dimension del tablero
 function extractDimension(form){
     var data = new FormData(form);
     for (const entry of data) {
@@ -276,6 +294,7 @@ document.getElementById("formColorPlayer1").addEventListener("change",function(e
 });
 //cambia los colores de la fichas del jugador 2
 document.getElementById("formColorPlayer2").addEventListener("change",function(e){
+    console.log("entra en el dos");
     if(!start){
         viewControl.hidenParagraph(pColor);
         fill2= viewControl.readColor(this);
