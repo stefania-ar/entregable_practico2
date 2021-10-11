798019bd69e1135cb534adf1640e41808c5582ff
Caseres Marina <marinacaseres1997@gmail.com>
2021-10-10 21:36:15 -0300

    Comentado archivo Piece.js

--- js/Piece.js
+++ js/Piece.js
@@ -37,15 +37,17 @@ class Piece{
     setInCell(inCell){
         this.inCell = inCell;
     }
-
+    //devuelve si la ficha se encuentra en el rango adecuado para caer dentro del tablero
     isPointInsideRange(x_ini,x_fin, y_ini, y_fin){
         return (this.getX()> x_ini && this.getX< x_fin) && (this.getY()> y_ini && this.getY< y_fin);
     }
-
+    //devuelve si el mouse entra al tablero por un lado inadecuado del tablero
     isInsideBoard(xMouse, yMouse, boardPosX, boardPosY, boardWidth, boardHeight, radio){
         return xMouse > boardPosX-radio && xMouse < boardPosX+boardWidth+radio && yMouse < boardPosY+boardHeight+radio && yMouse > boardPosY-radio;
     }
-
+    //cambia la posición de la ficha en caso que se arrastre por fuera del tablero
+    //se usa en caso que la ficha no esté en una celda y limita hasta donde se lleva la ficha
+    //para que las fichas de los jugadores no se mezclen
     setPosition(x, y, inCell, player, boardX, boardWidth, boardY, boardHeight){
         if(!inCell){
             if(player === 1 && x< boardX + boardWidth && y < boardY+boardHeight){
@@ -57,12 +59,13 @@ class Piece{
             }
         }
     }
-
+    //cambia la posicion de la ficha cuando se le aplica la transicion dentro del tabero
     setPositionOfTransition(xTransition, yTransition){
         this.x =xTransition;
         this.y =yTransition;
     }
-
+    //cambia la posición de la ficha de forma que no entre en el tablero
+    //se usa en caso que la ficha no esté en una celda y se quiera insertar por un lado del tablero que no corresponde
     setPositionOffBoard(layerX, layerY, boardPosX, boardPosY, boardWidth, radio, inCell){
         if(!inCell){
             if((this.x > boardPosX-radio && this.x < boardPosX+boardWidth+radio && this.y < boardPosY-radio) ||
